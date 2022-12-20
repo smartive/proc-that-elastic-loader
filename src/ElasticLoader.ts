fc5d5c3d05daf77e7e323b44696ed64f6fc12b47
Thilo Haas <thilo@smartive.ch>
2022-12-20 14:25:54 +0100

    feat: Update rxjs

--- src/ElasticLoader.ts
+++ src/ElasticLoader.ts
@@ -1,62 +1,58 @@
+import { Client, ClientOptions } from '@elastic/elasticsearch';
 import { Loader } from 'proc-that';
-import { Observable } from 'rxjs';
-
+import { EMPTY, from, Observable, throwError } from 'rxjs';
 import { Buffer } from './helpers/Buffer';
 
-const elasticsearch = require('elasticsearch');
-
 class NoIdProvidedError extends Error {
-    constructor(public object: any) {
-        super('No id provided by object');
-    }
+  constructor(public object: any) {
+    super('No id provided by object');
+  }
 }
 
 export class ElasticLoader implements Loader {
-    private esClient: any;
-    private buffer: Buffer<any> = new Buffer();
+  private esClient: Client;
+  private buffer: Buffer<any>;
 
-    constructor(
-        config: any,
-        private index: string,
-        private type: string,
-        private predicate: (obj: any) => boolean = () => true,
-        private idSelector: (obj: any) => any = o => o.id,
-    ) {
-        const esConfig = JSON.parse(JSON.stringify(config));
-        if (!esConfig.requestTimeout) {
-            // set requestTimeout to 5min.
-            // reason: when you shoot many index requests to the esClient, elasticsearch buffers your requests.
-            // after the default timeout of 30s is exceeded, you receive a TimeoutError and the whole index process fails.
-            esConfig.requestTimeout = 1000 * 60 * 5;
-        }
-        this.esClient = new elasticsearch.Client(esConfig);
-        if (esConfig.maxSockets) {
-            this.buffer = new Buffer(esConfig.maxSockets);
-        }
+  constructor(
+    config: ClientOptions,
+    private index: string,
+    private predicate: (obj: any) => boolean = () => true,
+    private idSelector: (obj: any) => any = (o) => o.id,
+    bufferSize = 10
+  ) {
+    const esConfig = JSON.parse(JSON.stringify(config)) as ClientOptions;
+    if (!esConfig.requestTimeout) {
+      // set requestTimeout to 5min.
+      // reason: when you shoot many index requests to the esClient, elasticsearch buffers your requests.
+      // after the default timeout of 30s is exceeded, you receive a TimeoutError and the whole index process fails.
+      esConfig.requestTimeout = 1000 * 60 * 5;
     }
 
-    public write(object: any): Observable<any> {
-        if (!this.predicate(object)) {
-            return Observable.empty();
-        }
+    this.buffer = new Buffer(bufferSize);
+    this.esClient = new Client(esConfig);
+  }
 
-        const id = this.idSelector(object);
-
-        if (id === null || id === undefined) {
-            return Observable.throw(new NoIdProvidedError(object));
-        }
-
-        const promise = this.buffer
-            .read()
-            .then(() => this.esClient.index({
-                id,
-                index: this.index,
-                type: this.type,
-                body: object,
-            }));
-
-        this.buffer.write(object);
-
-        return Observable.fromPromise(promise);
+  public write(object: any): Observable<any> {
+    if (!this.predicate(object)) {
+      return EMPTY;
     }
+
+    const id = this.idSelector(object);
+
+    if (id === null || id === undefined) {
+      return throwError(() => new NoIdProvidedError(object));
+    }
+
+    const promise = this.buffer.read().then(() =>
+      this.esClient.index({
+        id,
+        index: this.index,
+        body: object,
+      })
+    );
+
+    this.buffer.write(object);
+
+    return from(promise);
+  }
 }
--- test/Buffer.spec.ts
+++ test/Buffer.spec.ts
@@ -1,104 +1,98 @@
 import { Buffer } from '../src/helpers/Buffer';
 
 describe('Buffer<any>', () => {
+  let buf: Buffer<any>;
 
-    let buf: Buffer<any>;
+  beforeEach(() => {
+    buf = new Buffer<any>();
+  });
 
-    beforeEach(() => {
-        buf = new Buffer<any>();
+  it('should set size to default 10', () => {
+    expect(buf.size).toBe(10);
+  });
+
+  it('should extend EventEmitter', () => {
+    expect(buf.addListener).toBeDefined();
+  });
+
+  it('should be empty on init', () => {
+    expect(buf.isEmpty).toBeTruthy();
+  });
+
+  it('should not be full on init', () => {
+    expect(buf.isFull).toBeFalsy();
+  });
+
+  it('should change size with property', () => {
+    expect(buf.size).toMatchSnapshot();
+    buf.size = 20;
+    expect(buf.size).toMatchSnapshot();
+  });
+
+  describe('read()', () => {
+    it('should return a Promise', () => {
+      buf.write('');
+      expect(buf.read()).toBeInstanceOf(Promise);
     });
 
-    it('should set size to default 10', () => {
-        expect(buf.size).toBe(10);
+    it('should not resolve without write while empty', (done) => {
+      buf.read().then(() => {
+        done(new Error('promise returned'));
+      });
+
+      setTimeout(() => {
+        done();
+      }, 500);
     });
 
-    it('should extend EventEmitter', () => {
-        expect(buf.addListener).toBeDefined();
+    it('should resolve on write', async () => {
+      expect(buf.read()).resolves.toBe('hello');
+      await buf.write('hello');
     });
 
-    it('should be empty on init', () => {
-        expect(buf.isEmpty).toBeTruthy();
+    it('should emit release event', () => {
+      const spy = jest.fn();
+      buf.on('release', spy);
+      buf.read();
+      buf.write('');
+      expect(spy.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('write()', () => {
+    beforeEach((done) => {
+      buf.size = 1;
+      buf.write('hello').then(() => done());
     });
 
-    it('should not be full on init', () => {
-        expect(buf.isFull).toBeFalsy();
+    it('should return a Promise', () => {
+      buf.read();
+      expect(buf.write('')).toBeInstanceOf(Promise);
     });
 
-    it('should change size with property', () => {
-        expect(buf.size).toMatchSnapshot();
-        buf.size = 20;
-        expect(buf.size).toMatchSnapshot();
+    it('should not resolve without read while full', (done) => {
+      buf.write('world').then(() => {
+        done(new Error('promise returned'));
+      });
+
+      setTimeout(() => {
+        done();
+      }, 500);
     });
 
-    describe('read()', () => {
+    it('should resolve on read', async () => {
+      buf.write('world');
 
-        it('should return a Promise', () => {
-            buf.write('');
-            expect(buf.read()).toBeInstanceOf(Promise);
-        });
-
-        it('should not resolve without write while empty', done => {
-            buf.read().then(obj => {
-                done(new Error('promise returned'));
-            });
-
-            setTimeout(() => {
-                done();
-            }, 500);
-        });
-
-        it('should resolve on write', async () => {
-            expect(buf.read()).resolves.toBe('hello');
-            await buf.write('hello');
-        });
-
-        it('should emit release event', () => {
-            const spy = jest.fn();
-            buf.on('release', spy);
-            buf.read();
-            buf.write('');
-            expect(spy.mock.calls.length).toBe(1);
-        });
-
+      expect(await buf.read()).toBe('hello');
+      expect(await buf.read()).toBe('world');
     });
 
-    describe('write()', () => {
-
-        beforeEach(done => {
-            buf.size = 1;
-            buf.write('hello').then(() => done());
-        });
-
-        it('should return a Promise', () => {
-            buf.read();
-            expect(buf.write('')).toBeInstanceOf(Promise);
-        });
-
-        it('should not resolve without read while full', done => {
-            buf.write('world').then(obj => {
-                done(new Error('promise returned'));
-            });
-
-            setTimeout(() => {
-                done();
-            }, 500);
-        });
-
-        it('should resolve on read', async () => {
-            buf.write('world');
-
-            expect(await buf.read()).toBe('hello');
-            expect(await buf.read()).toBe('world');
-        });
-
-        it('should emit write event', () => {
-            let spy = jest.fn();
-            buf.on('write', spy);
-            buf.read();
-            buf.write('');
-            expect(spy.mock.calls.length).toBe(1);
-        });
-
+    it('should emit write event', () => {
+      const spy = jest.fn();
+      buf.on('write', spy);
+      buf.read();
+      buf.write('');
+      expect(spy.mock.calls.length).toBe(1);
     });
-
+  });
 });
