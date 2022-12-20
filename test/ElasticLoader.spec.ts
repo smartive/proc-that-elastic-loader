4ff984ee05933af31fa491b66fd3fa6024390832
Thilo Haas <thilo@thilohaas.ch>
2022-12-20 14:35:46 +0100

    feat: Update rxjs (#42)
    BREAKING CHANGE:
    - Removed support for rxjs v5, minimum version is rxjs v7
    - Removed support for Node.js v6, minimum version is v16
    - Removed support for proc-that v1, minimum version is v2
    - Removed support for old elasticsearch version, minimum version is v7.x

--- test/ElasticLoader.spec.ts
+++ test/ElasticLoader.spec.ts
@@ -1,140 +1,166 @@
-import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs';
 
 import { ElasticLoader } from '../src/ElasticLoader';
 
 describe('ElasticLoader', () => {
+  let loader: ElasticLoader;
+  let client: any;
 
-    let loader: ElasticLoader;
-    let client: any;
+  beforeEach(() => {
+    client = {
+      index: (o) => of(o),
+    };
 
-    beforeEach(() => {
-        client = {
-            index: o => Observable.of(o)
-        };
+    client.index = jest.fn(client.index);
+  });
 
-        client.index = jest.fn(client.index);
+  it('should resolve on correct usage', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex');
+    (loader as any).esClient = client;
+
+    loader.write({ id: 1, text: 'test' }).subscribe({ error: done, complete: done });
+  });
+
+  it('should use correct index', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex');
+    (loader as any).esClient = client;
+
+    loader.write({ id: 1, text: 'test' }).subscribe({
+      error: done,
+      complete: () => {
+        try {
+          expect(client.index.mock.calls.length).toBe(1);
+          expect(client.index.mock.calls[0][0]).toMatchSnapshot();
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
     });
+  });
 
-    it('should resolve on correct usage', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType');
-        (loader as any).esClient = client;
+  it('should use correct type', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex');
+    (loader as any).esClient = client;
 
-        loader.write({id: 1, text: 'test'}).subscribe(null, done, done);
+    loader.write({ id: 1, text: 'test' }).subscribe({
+      error: done,
+      complete: () => {
+        try {
+          expect(client.index.mock.calls.length).toBe(1);
+          expect(client.index.mock.calls[0][0]).toMatchSnapshot();
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
     });
+  });
 
-    it('should use correct index', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType');
-        (loader as any).esClient = client;
+  it('should use correct id', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex');
+    (loader as any).esClient = client;
 
-        loader.write({id: 1, text: 'test'}).subscribe(null, done, () => {
-            try {
-                expect(client.index.mock.calls.length).toBe(1);
-                expect(client.index.mock.calls[0][0]).toMatchSnapshot();
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
+    loader.write({ id: 1, text: 'test' }).subscribe({
+      error: done,
+      complete: () => {
+        try {
+          expect(client.index.mock.calls.length).toBe(1);
+          expect(client.index.mock.calls[0][0]).toMatchSnapshot();
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
     });
+  });
 
-    it('should use correct type', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType');
-        (loader as any).esClient = client;
+  it('should use correct body', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex');
+    (loader as any).esClient = client;
 
-        loader.write({id: 1, text: 'test'}).subscribe(null, done, () => {
-            try {
-                expect(client.index.mock.calls.length).toBe(1);
-                expect(client.index.mock.calls[0][0]).toMatchSnapshot();
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
+    loader.write({ id: 1, text: 'test' }).subscribe({
+      error: done,
+      complete: () => {
+        try {
+          expect(client.index.mock.calls.length).toBe(1);
+          expect(client.index.mock.calls[0][0]).toMatchSnapshot();
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
     });
+  });
 
-    it('should use correct id', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType');
-        (loader as any).esClient = client;
+  it('should be called on truthly predicate', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex', (o) => o.text === 'test');
+    (loader as any).esClient = client;
 
-        loader.write({id: 1, text: 'test'}).subscribe(null, done, () => {
-            try {
-                expect(client.index.mock.calls.length).toBe(1);
-                expect(client.index.mock.calls[0][0]).toMatchSnapshot();
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
+    loader.write({ id: 1, text: 'test' }).subscribe({
+      error: done,
+      complete: () => {
+        try {
+          expect(client.index.mock.calls.length).toBe(1);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
     });
+  });
 
-    it('should use correct body', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType');
-        (loader as any).esClient = client;
+  it('should not called on falsly predicate', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex', (o) => o.text !== 'test');
+    (loader as any).esClient = client;
 
-        loader.write({id: 1, text: 'test'}).subscribe(null, done, () => {
-            try {
-                expect(client.index.mock.calls.length).toBe(1);
-                expect(client.index.mock.calls[0][0]).toMatchSnapshot();
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
+    loader.write({ id: 1, text: 'test' }).subscribe({
+      error: done,
+      complete: () => {
+        try {
+          expect(client.index.mock.calls.length).toBe(0);
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
     });
+  });
 
-    it('should be called on truthly predicate', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType', o => o.text === 'test');
-        (loader as any).esClient = client;
+  it('should be called with correct id when using custom selector', (done) => {
+    loader = new ElasticLoader(
+      { nodes: ['http://localhost:9200'] },
+      'testIndex',
+      () => true,
+      (o) => o.myId
+    );
+    (loader as any).esClient = client;
 
-        loader.write({id: 1, text: 'test'}).subscribe(null, done, () => {
-            try {
-                expect(client.index.mock.calls.length).toBe(1);
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
+    loader.write({ myId: 1, text: 'test' }).subscribe({
+      error: done,
+      complete: () => {
+        try {
+          expect(client.index.mock.calls.length).toBe(1);
+          expect(client.index.mock.calls[0][0]).toMatchSnapshot();
+          done();
+        } catch (e) {
+          done(e);
+        }
+      },
     });
+  });
 
-    it('should not called on falsly predicate', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType', o => o.text !== 'test');
-        (loader as any).esClient = client;
+  it('should reject when no id is provided', (done) => {
+    loader = new ElasticLoader({ nodes: ['http://localhost:9200'] }, 'testIndex');
+    (loader as any).esClient = client;
 
-        loader.write({id: 1, text: 'test'}).subscribe(null, done, () => {
-            try {
-                expect(client.index.mock.calls.length).toBe(0);
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
+    loader.write({ myId: 1, text: 'test' }).subscribe({
+      error: () => {
+        done();
+      },
+      complete: () => {
+        done(new Error('did not throw.'));
+      },
     });
-
-    it('should be called with correct id when using custom selector', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType', o => true, o => o.myId);
-        (loader as any).esClient = client;
-
-        loader.write({myId: 1, text: 'test'}).subscribe(null, done, () => {
-            try {
-                expect(client.index.mock.calls.length).toBe(1);
-                expect(client.index.mock.calls[0][0]).toMatchSnapshot();
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-    });
-
-    it('should reject when no id is provided', done => {
-        loader = new ElasticLoader({}, 'testIndex', 'testType');
-        (loader as any).esClient = client;
-
-        loader.write({myId: 1, text: 'test'}).subscribe(null, () => {
-            done();
-        }, () => {
-            done(new Error('did not throw.'));
-        })
-    });
-
-})
-;
+  });
+});
